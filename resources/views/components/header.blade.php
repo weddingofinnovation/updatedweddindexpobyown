a29caf2e62fa3c48dd15cfe77b2220c2fb38ff90
kuchnhiarha <exhibitionnetwork@gmail.com>
2026-05-14 22:24:52 +0530

    140520262224

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -5,13 +5,15 @@
                 <div class="h-8 w-px bg-gray-100"></div>
                 <div class="flex flex-col">
                     <span class="text-[7px] font-black uppercase tracking-widest text-[#ED8B00]">Save the Date</span>
-                    <span class="text-[18px] font-bold text-black whitespace-nowrap">7 – 9 Aug 2026</span>
+                    <span class="text-[18px] font-bold text-black whitespace-nowrap">05 – 07</span>
+                    <span class="text-[18px] font-bold text-black whitespace-nowrap"> MARCH 2027</span>
                 </div>
             </div>
         </div>
 
-        <div class="hidden md:flex items-center gap-5 h-full">
-            <div class="relative group h-full flex items-center"><button class="text-[14px] font-black uppercase tracking-wider text-gray-900 flex items-center gap-1 group-hover:text-[#ED8B00] transition-colors relative py-1">Understanding<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down transition-transform duration-200 group-hover:rotate-180">
+        <!-- <div class="hidden md:flex items-center gap-5 h-full">
+            <div class="relative group h-full flex items-center"><button class="text-[14px] font-black uppercase tracking-wider text-gray-900 flex items-center gap-1 group-hover:text-[#ED8B00] transition-colors relative py-1">
+                Understanding<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down transition-transform duration-200 group-hover:rotate-180">
                         <path d="m6 9 6 6 6-6"></path>
                     </svg>
                     <div class="absolute -bottom-2 left-0 w-0 h-[2px] bg-[#ED8B00] transition-all duration-200 group-hover:w-full"></div>
@@ -41,6 +43,100 @@
                 </div>
                 </div>
             </div><a class="text-[14px] font-black uppercase tracking-wider text-gray-900 hover:text-[#ED8B00] transition-colors relative py-1 group" href="/floor-plan">Expo Map<div class="absolute -bottom-2 left-0 w-0 h-[2px] bg-[#ED8B00] transition-all duration-200 group-hover:w-full"></div></a><a class="text-[14px] font-black uppercase tracking-wider text-gray-900 hover:text-[#ED8B00] transition-colors relative py-1 group" href="/news">News<div class="absolute -bottom-2 left-0 w-0 h-[2px] bg-[#ED8B00] transition-all duration-200 group-hover:w-full"></div></a><a class="text-[14px] font-black uppercase tracking-wider text-gray-900 hover:text-[#ED8B00] transition-colors relative py-1 group" href="https://weddingindiaexpo.com/discover/exhibitors">Discover<div class="absolute -bottom-2 left-0 w-0 h-[2px] bg-[#ED8B00] transition-all duration-200 group-hover:w-full"></div></a>
+        </div> -->
+
+        <div class="hidden md:flex items-center gap-5 h-full">
+            <div class="relative group h-full flex items-center">
+                <button class="text-[14px] font-black uppercase tracking-wider text-gray-900 flex items-center gap-1 group-hover:text-[#ED8B00] transition-colors relative py-1">
+                The Event<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down transition-transform duration-200 group-hover:rotate-180">
+                        <path d="m6 9 6 6 6-6"></path>
+                    </svg>
+                    <div class="absolute -bottom-2 left-0 w-0 h-[2px] bg-[#ED8B00] transition-all duration-200 group-hover:w-full"></div>
+                </button>
+
+                <div class="absolute top-[calc(100%-10px)] left-1/2 -translate-x-1/2 w-[260px] bg-white shadow-[0_20px_50px_rgba(0,0,0,0.15)] rounded-xl border border-gray-50 py-4 transition-all duration-300 origin-top z-50 opacity-0 scale-95 pointer-events-none group-hover:opacity-100 group-hover:scale-100 group-hover:pointer-events-auto">
+                    <div class="grid gap-1 px-2">
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/understanding-expo/participants/benefits">Benefits as an Exhibitor</a>
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/understanding-expo/participants/application">Exhibitor Application</a>
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/understanding-expo/opportunity-district/individual-participation">Individual Participation</a>
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/understanding-expo/opportunity-district/request-free-stand">Request Free Stand Area</a>
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/understanding-expo/opportunity-district/new-product-showcase">New Product Showcase</a>
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/understanding-expo/expo-initiatives">Expo Initiatives</a>
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/understanding-expo/sponsorship-plan">Sponsorship Plan</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="relative group h-full flex items-center">
+                <button class="text-[14px] font-black uppercase tracking-wider text-gray-900 flex items-center gap-1 group-hover:text-[#ED8B00] transition-colors relative py-1">Visitors<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down transition-transform duration-200 group-hover:rotate-180">
+                        <path d="m6 9 6 6 6-6"></path>
+                    </svg>
+                    <div class="absolute -bottom-2 left-0 w-0 h-[2px] bg-[#ED8B00] transition-all duration-200 group-hover:w-full"></div>
+                </button>
+                
+                <div class="absolute top-[calc(100%-10px)] left-1/2 -translate-x-1/2 w-[260px] bg-white shadow-[0_20px_50px_rgba(0,0,0,0.15)] rounded-xl border border-gray-50 py-4 transition-all duration-300 origin-top z-50 opacity-0 scale-95 pointer-events-none group-hover:opacity-100 group-hover:scale-100 group-hover:pointer-events-auto">
+                    <div class="grid gap-1 px-2">
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/experiences/international-destination-pavilion">International Destination Pavilion</a>
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/experiences/india-destination-pavilion">India Destination Pavilion</a>
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/experiences/luxury-resort-hotel-pavilion">Luxury Resort &amp; Hotel Pavilion</a>
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/experiences/honeymoon-travel-zone">Honeymoon &amp; Travel Zone</a>
+
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/experiences/food-and-livelihoods">Food &amp; Livelihoods</a>
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/experiences/wedding-cuisine-catering-sweets-pavilion">Wedding Cuisine &amp; Catering, Sweets</a>
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/experiences/cultural-wedding-showcase-arena">Cultural Wedding Fashion Arena</a>
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/experiences/gifting-showcase-area">Gifting &amp; Showcase Area</a>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="relative group h-full flex items-center">
+                <button class="text-[14px] font-black uppercase tracking-wider text-gray-900 flex items-center gap-1 group-hover:text-[#ED8B00] transition-colors relative py-1">EXhibitor<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down transition-transform duration-200 group-hover:rotate-180">
+                        <path d="m6 9 6 6 6-6"></path>
+                    </svg>
+                    <div class="absolute -bottom-2 left-0 w-0 h-[2px] bg-[#ED8B00] transition-all duration-200 group-hover:w-full"></div>
+                </button>
+                
+                <div class="absolute top-[calc(100%-10px)] left-1/2 -translate-x-1/2 w-[260px] bg-white shadow-[0_20px_50px_rgba(0,0,0,0.15)] rounded-xl border border-gray-50 py-4 transition-all duration-300 origin-top z-50 opacity-0 scale-95 pointer-events-none group-hover:opacity-100 group-hover:scale-100 group-hover:pointer-events-auto">
+                    <div class="grid gap-1 px-2">
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/experiences/international-destination-pavilion">International Destination Pavilion</a>
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/experiences/india-destination-pavilion">India Destination Pavilion</a>
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/experiences/luxury-resort-hotel-pavilion">Luxury Resort &amp; Hotel Pavilion</a>
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/experiences/honeymoon-travel-zone">Honeymoon &amp; Travel Zone</a>
+
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/experiences/food-and-livelihoods">Food &amp; Livelihoods</a>
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/experiences/wedding-cuisine-catering-sweets-pavilion">Wedding Cuisine &amp; Catering, Sweets</a>
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/experiences/cultural-wedding-showcase-arena">Cultural Wedding Fashion Arena</a>
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/experiences/gifting-showcase-area">Gifting &amp; Showcase Area</a>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="relative group h-full flex items-center">
+                <button class="text-[14px] font-black uppercase tracking-wider text-gray-900 flex items-center gap-1 group-hover:text-[#ED8B00] transition-colors relative py-1">Media Centre<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down transition-transform duration-200 group-hover:rotate-180">
+                        <path d="m6 9 6 6 6-6"></path>
+                    </svg>
+                    <div class="absolute -bottom-2 left-0 w-0 h-[2px] bg-[#ED8B00] transition-all duration-200 group-hover:w-full"></div>
+                </button>
+                
+                <div class="absolute top-[calc(100%-10px)] left-1/2 -translate-x-1/2 w-[260px] bg-white shadow-[0_20px_50px_rgba(0,0,0,0.15)] rounded-xl border border-gray-50 py-4 transition-all duration-300 origin-top z-50 opacity-0 scale-95 pointer-events-none group-hover:opacity-100 group-hover:scale-100 group-hover:pointer-events-auto">
+                    <div class="grid gap-1 px-2">
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/experiences/international-destination-pavilion">International Destination Pavilion</a>
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/experiences/india-destination-pavilion">India Destination Pavilion</a>
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/experiences/luxury-resort-hotel-pavilion">Luxury Resort &amp; Hotel Pavilion</a>
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/experiences/honeymoon-travel-zone">Honeymoon &amp; Travel Zone</a>
+
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/experiences/food-and-livelihoods">Food &amp; Livelihoods</a>
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/experiences/wedding-cuisine-catering-sweets-pavilion">Wedding Cuisine &amp; Catering, Sweets</a>
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/experiences/cultural-wedding-showcase-arena">Cultural Wedding Fashion Arena</a>
+                        <a class="block px-6 py-2.5 text-[13px] font-bold text-gray-700 hover:bg-slate-50 hover:text-[#ED8B00] rounded-lg transition-all" href="/experiences/gifting-showcase-area">Gifting &amp; Showcase Area</a>
+
+                    </div>
+                </div>
+            </div>
+
+             <a class="text-[14px] font-black uppercase tracking-wider text-gray-900 hover:text-[#ED8B00] transition-colors relative py-1 group" href="/floor-plan">Expo Map<div class="absolute -bottom-2 left-0 w-0 h-[2px] bg-[#ED8B00] transition-all duration-200 group-hover:w-full"></div></a><a class="text-[14px] font-black uppercase tracking-wider text-gray-900 hover:text-[#ED8B00] transition-colors relative py-1 group" href="/news">News<div class="absolute -bottom-2 left-0 w-0 h-[2px] bg-[#ED8B00] transition-all duration-200 group-hover:w-full"></div></a><a class="text-[14px] font-black uppercase tracking-wider text-gray-900 hover:text-[#ED8B00] transition-colors relative py-1 group" href="https://weddingindiaexpo.com/discover/exhibitors">Discover<div class="absolute -bottom-2 left-0 w-0 h-[2px] bg-[#ED8B00] transition-all duration-200 group-hover:w-full"></div></a>
         </div>
 
         <div class="hidden md:flex items-center gap-1">
